ddece20cd2d31a678ce664b9f27a05715e81980a
Kevin Wonus <sales@wonus.com>
2021-05-05 23:18:17 -0400

    Add project files.

---
+++ Ribbon.cs
@@ -0,0 +1,242 @@
+﻿using AVX.Properties;
+using Microsoft.Office.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using Office = Microsoft.Office.Core;
+using System.Drawing;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+using System.Windows.Resources;
+
+// TODO:  Follow these steps to enable the Ribbon (XML) item:
+
+// 1: Copy the following code block into the ThisAddin, ThisWorkbook, or ThisDocument class.
+
+//  protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+//  {
+//      return new Ribbon();
+//  }
+
+// 2. Create callback methods in the "Ribbon Callbacks" region of this class to handle user
+//    actions, such as clicking a button. Note: if you have exported this Ribbon from the Ribbon designer,
+//    move your code from the event handlers to the callback methods and modify the code to work with the
+//    Ribbon extensibility (RibbonX) programming model.
+
+// 3. Assign attributes to the control tags in the Ribbon XML file to identify the appropriate callback methods in your code.  
+
+// For more information, see the Ribbon XML documentation in the Visual Studio Tools for Office Help.
+
+
+namespace AVX
+{
+    [ComVisible(true)]
+    public class Ribbon : Office.IRibbonExtensibility
+    {
+        private Office.IRibbonUI ribbon;
+        public (UInt32 idx, byte ccnt)[] bkIdx { get; private set; }
+        public Dictionary<byte, Dictionary<byte, (UInt32 writIdx, byte vcnt)>> chIdx { get; private set; }
+        public (UInt16 word, byte punc, byte tx)[] writ { get; private set; }
+
+        public Dictionary<UInt16, string> Search;
+        public Dictionary<UInt16, string> Display;
+        public Dictionary<UInt16, string> Modern;
+
+        private ThisAddIn avx;
+        public static Ribbon RIBBON { get; private set; } = null;
+        public static ThisAddIn AVX { get; private set; } = null;
+        public Ribbon(ThisAddIn avx)
+        {
+            Ribbon.RIBBON = this;
+            this.avx = avx;
+            Ribbon.AVX = this.avx;
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            var path = "C:/src/Digital-AV/z-series/AV-Writ-32.dx";
+            var file = File.OpenRead(path);
+
+            var len = file.Length;
+            var cnt = (UInt32) (len / 4);
+            if (len != (0xC0C93) * 4)
+                MessageBox.Show(null, "AVX Addin for Microsoft Word", "Possible File corruption error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            this.writ = new (UInt16 word, byte punc, byte tx)[cnt];
+            this.bkIdx = new (UInt32 idx, byte ccnt)[1 + 66];  // waste element-zero for a one-based index
+            this.chIdx = new Dictionary<byte, Dictionary<byte, (UInt32 writIdx, byte vcnt)>>();
+            for (byte num = 1; num <= 66; num++)
+                this.chIdx[num] = new Dictionary<byte, (UInt32 writIdx, byte vcnt)>();
+
+            byte b = 0;
+            byte c = 0;
+            byte v = 0;
+
+            var reader = new BinaryReader(file);
+            UInt32 cidx = 0;
+            UInt32 vidx = 0;
+            (UInt16 word, byte punc, byte tx) record;
+            for (UInt32 i = 0; i < cnt; i++)
+            {
+                record.word = reader.ReadUInt16();
+                record.punc = reader.ReadByte();
+                record.tx   = reader.ReadByte();
+                this.writ[i] = record;
+
+                switch (record.tx & 0xF0)
+                {
+                    case TX.BoV:
+                        v++;
+                        break;
+                    case TX.BoC:
+                        v = 1;
+                        c++;
+                        cidx = i;
+                        break;
+                    case TX.BoB:
+                        v = 1;
+                        c = 1;
+                        b++;
+                        cidx = i;
+                        break;
+                    case TX.EoC:
+                        this.chIdx[b][c] = (cidx, v);
+                        break;
+                    case TX.EoB:
+                        this.chIdx[b][c] = (cidx, v);
+                        this.bkIdx[b] = (vidx, c);
+                        break;
+                }
+            }
+            reader.Close();
+            file.Close();
+
+            path = "C:/src/Digital-AV/z-series/AV-Lexicon.dxi";
+            this.Search = new Dictionary<UInt16, string>();
+            this.Display = new Dictionary<UInt16, string>();
+            this.Modern = new Dictionary<UInt16, string>();
+
+            file = File.OpenRead(path);
+            reader = new BinaryReader(file);
+            char[] word = new char[24];
+
+            for (UInt16 key = 1; key <= 12567; key++)
+            {
+                reader.ReadUInt16();
+                var size = reader.ReadUInt16();
+                if (size == 12567)
+                    break;
+                UInt32 ignore;
+                for (int s = 0; s < size; s++)
+                    ignore = reader.ReadUInt32();
+                for (int w = 1; w <= 3; w++)
+                {
+                    for (int i = 0; i < word.Length; i++)
+                    {
+                        word[i] = (char)file.ReadByte();
+                        if (word[i] == (char) 0)
+                        {
+                            if (i > 0)
+                            {
+                                var text = new string(word).Substring(0,i);
+                                switch (w)
+                                {
+                                    case 1: Search[key]  = text; break;
+                                    case 2: Display[key] = text; break;
+                                    case 3: Modern[key]  = text; break;
+                                }
+                            }
+                            break;
+                        }
+                    }
+                }
+
+            }
+            reader.Close();
+            file.Close();
+
+        }
+
+        #region IRibbonExtensibility Members
+
+        public string GetCustomUI(string ribbonID)
+        {
+            var xml = GetResourceText("AVX.Ribbon.xml");
+            return xml;
+        }
+        private string root = @"C:\Users\kevin\OneDrive\Documents\";
+        private Bitmap BitmapAVX = null;
+        public Bitmap GetImageAVX(IRibbonControl control)
+        { 
+            return (BitmapAVX != null) ? BitmapAVX : BitmapAVX = new Bitmap(root + "avx64.png");
+        }
+        public void clickButtonAVX(Office.IRibbonControl control)
+        {
+            try
+            {
+//              Word.Range rng = this.avx.Application.ActiveDocument.Range(0, 0);
+//              rng.Text = "New Text ";
+                var popup = new SelectVerse();
+                popup.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(null, "Please open a document prior to inserting verses", "Cannot insert text with a current document", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        public void clickBookAVX(Office.IRibbonControl control)
+        {
+            try
+            {
+                var num = control.Id.Substring(2);
+                var bk = UInt16.Parse(num);
+//              MessageBox.Show(null, "Book-" + num + " = " + control.Tag, "Book Selection", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                var popup = new SelectVerse((byte)bk);
+                popup.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(null, "Please open a document prior to inserting verses", "Cannot insert text with a current document", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        #endregion
+
+        #region Ribbon Callbacks
+        //Create callback methods here. For more information about adding callback methods, visit https://go.microsoft.com/fwlink/?LinkID=271226
+
+        public void Ribbon_Load(Office.IRibbonUI ribbonUI)
+        {
+            this.ribbon = ribbonUI;
+        }
+
+        #endregion
+
+        #region Helpers
+
+        private static string GetResourceText(string resourceName)
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            string[] resourceNames = asm.GetManifestResourceNames();
+            for (int i = 0; i < resourceNames.Length; ++i)
+            {
+                if (string.Compare(resourceName, resourceNames[i], StringComparison.OrdinalIgnoreCase) == 0)
+                {
+                    using (StreamReader resourceReader = new StreamReader(asm.GetManifestResourceStream(resourceNames[i])))
+                    {
+                        if (resourceReader != null)
+                        {
+                            return resourceReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        #endregion
+    }
+}
---
+++ SelectVerse.xaml.cs
@@ -0,0 +1,367 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace AVX
+{
+    public abstract class CAPS
+    {
+        public const UInt16 Cap1st  = 0x8000;
+        public const UInt16 CapAll  = 0x4000;
+        public const UInt16 CapAny  = 0xC000;
+        public const UInt16 CapNone = 0x0000;
+
+        public static string Captitalize(UInt16 cap, string input)
+        {
+            switch (cap & CapAny)
+            {
+                case Cap1st:  return input.Substring(0,1).ToUpper() + ((input.Length > 0) ? input.Substring(1) : "");
+                case CapAll:  return input.ToUpper();
+                default:      return input;
+            }
+        }
+    }
+    public abstract class TX
+    {
+        public const UInt16 BoV = 0x20;
+        public const UInt16 BoC = 0x60;
+        public const UInt16 BoB = 0xE0;
+
+        public const UInt16 EoC = 0x70;
+        public const UInt16 EoB = 0xF0;
+    }
+    public abstract class PUNC
+    {
+        public const UInt16 CLAUSE = 0xE0;
+        public const UInt16 Exclamatory = 0x80;
+        public const UInt16 Interrogative = 0xC0;
+        public const UInt16 Declarative = 0xE0;
+        public const UInt16 Dash = 0xA0;
+        public const UInt16 Semicolon = 0x20;
+        public const UInt16 Comma = 0x40;
+        public const UInt16 Colon = 0x60;
+        public const UInt16 Possessive = 0x10;
+        public const UInt16 ParenClose = 0x0C;
+        public const UInt16 Parenthetical = 0x04;
+        public const UInt16 Italics = 0x02;
+        public const UInt16 Jesus = 0x01;
+
+        public static string PostfixPunctuation(string word, byte punc)
+        {
+            string postfix;
+            if ((punc & Possessive) == Possessive)
+                postfix = word != null && word.EndsWith("s") ? "'" : "'s";
+            else
+                postfix = "";
+
+            if ((punc & ParenClose) == ParenClose)
+                postfix += ")";
+
+            switch (punc & CLAUSE)
+            {
+                case Exclamatory:   return postfix + "!";
+                case Interrogative: return postfix + "?";
+                case Declarative:   return postfix + ".";
+                case Dash:          return postfix + "--";
+                case Semicolon:     return postfix + ";";
+                case Comma:         return postfix + ",";
+                case Colon:         return postfix + ":";
+            }
+            return postfix;
+        }
+        public static string PrefixPunctuation(byte punc, byte prev)
+        {
+            if ((punc & Parenthetical) == Parenthetical && (prev & Parenthetical) != Parenthetical)
+                return "(";
+            return "";
+        }
+        public static bool IsItalisized(byte punc)
+        {
+            return ((punc & Italics) == Italics);
+        }
+    }
+    /// <summary>
+    /// Interaction logic for SelectVerse.xaml
+    /// </summary>
+    public partial class SelectVerse : Window
+    {
+        private void WriteVerseSpec()
+        {
+            ComboBoxItem item = (ComboBoxItem)this.comboBoxBook.SelectedItem;
+            var bk = item.Tag.ToString().Substring(2);
+            var b = (byte)UInt16.Parse(bk);
+            var book = Ribbon.RIBBON.bkIdx[b];
+            string spec = item.Content.ToString() + " " + this.textBoxChaterAndVerse.Text.Trim().Replace(" ", "").Replace(",", ", ") + "\n";
+            dynamic rng = Ribbon.AVX.Application.ActiveDocument.Range();
+            rng.Collapse(Word.WdCollapseDirection.wdCollapseEnd);
+            rng.Bold = 1;
+            rng.Text = spec;
+            foreach (Word.Range w in rng.Words)
+            {
+                w.Font.Bold = 1;
+            }
+        }
+        private void WriteVerse(byte b, byte c, byte v, bool modern, bool contiguous)
+        {
+            byte prevPunc = 0;
+            var chapter = Ribbon.RIBBON.chIdx[b][c];
+            var records = Ribbon.RIBBON.writ;
+            UInt32 r = chapter.writIdx;
+            for (int i = 1; i < v; /**/)
+            {
+                r++;
+                if ((records[r].tx & 0x70) == 0x20) // BoV
+                    i++;
+                if ((records[r].tx & 0x70) == 0x70) // EoC or EoB
+                    return;
+            }
+            var first = true;
+            var verse = new StringBuilder();
+            if (!contiguous)
+                verse.Append("\n");
+            do
+            {
+                string word = null;
+                UInt16 key = (UInt16)(0x7FFF & records[r].word);
+                if (modern && Ribbon.RIBBON.Modern.ContainsKey(key))
+                    word = Ribbon.RIBBON.Modern[key];
+                if (word == null && Ribbon.RIBBON.Display.ContainsKey(key))
+                    word = Ribbon.RIBBON.Display[key];
+                if (word == null && Ribbon.RIBBON.Search.ContainsKey(key))
+                    word = Ribbon.RIBBON.Search[key];
+
+                if (first)
+                    first = false;
+                else
+                    verse.Append(' ');
+
+                var postfix = PUNC.PostfixPunctuation(word, records[r].punc);
+                var prefix = PUNC.PrefixPunctuation(records[r].punc, prevPunc);
+                prevPunc = records[r].punc;
+
+                if (prefix.Length > 0)
+                    verse.Append(prefix);
+                verse.Append(CAPS.Captitalize(records[r].word, word));
+                if (postfix.Length > 0)
+                    verse.Append(postfix);
+
+            }   while ((records[r++].tx & 0x30) != 0x30); // EoV or EoC or EoB
+
+            verse.Append("  ");
+
+            dynamic rng = Ribbon.AVX.Application.ActiveDocument.Range();
+            rng.Collapse(Word.WdCollapseDirection.wdCollapseEnd);
+            rng.Text = verse.ToString();
+        }
+       public SelectVerse(byte bkNum)
+        {
+            InitializeComponent();
+            if (bkNum >= 1 && bkNum <= 66)
+                this.comboBoxBook.SelectedItem = this.comboBoxBook.Items.GetItemAt(bkNum - 1);
+
+        }
+        public SelectVerse()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (this.comboBoxBook.SelectedItem != null)
+            {
+                ComboBoxItem item = (ComboBoxItem) this.comboBoxBook.SelectedItem;
+                var bk = item.Tag.ToString().Substring(2);
+                var b = (byte) UInt16.Parse(bk);
+                var book = Ribbon.RIBBON.bkIdx[b];
+                string bname = item.Content.ToString();
+                this.info.Text = bname + " has " + ((uint)book.ccnt).ToString() + " chapters";
+            }
+        }
+
+        private void textBoxChaterAndVerse_KeyUp(object sender, KeyEventArgs e)
+        {
+            ComboBoxItem item = (ComboBoxItem)this.comboBoxBook.SelectedItem;
+            var bk = item.Tag.ToString().Substring(2);
+            var b = (byte)UInt16.Parse(bk);
+            var book = Ribbon.RIBBON.bkIdx[b];
+            string bname = item.Content.ToString();
+            this.info.Text = bname + " has " + ((uint)book.ccnt).ToString() + " chapters";
+
+            if (this.textBoxChaterAndVerse.Text.Length > 0)
+            {
+                var text = this.textBoxChaterAndVerse.Text.Trim();
+                var len = text.Length;
+
+                UInt16 ch = 0;
+                for (int i = 0; i < len; i++)
+                {
+                    if (text[i] >= '0' && text[i] <= '9')
+                    {
+                        ch *= 10;
+                        ch += (byte)((byte)text[i] - (byte)'0');
+                    }
+                    else break;
+                }
+                if (ch <= 255)
+                {
+                    byte c = (byte)ch;
+                    if (Ribbon.RIBBON.chIdx[b].ContainsKey(c))
+                    {
+                        var chapter = Ribbon.RIBBON.chIdx[b][c];
+                        this.info.Text += ("\nChapter " + ch.ToString() + " has " + ((uint)chapter.vcnt).ToString() + " verses");
+                    }
+                }
+            }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            ComboBoxItem item = (ComboBoxItem)this.comboBoxBook.SelectedItem;
+            if (item != null)
+            {
+                string bad = null;
+                string error = null;
+
+                var list = new List<byte>();
+
+                var bk = item.Tag.ToString().Substring(2);
+                var b = (byte)UInt16.Parse(bk);
+                var book = Ribbon.RIBBON.bkIdx[b];
+                string bname = item.Content.ToString();
+                byte c = 0;
+
+                if (this.textBoxChaterAndVerse.Text.Length > 0)
+                {
+                    var text = this.textBoxChaterAndVerse.Text.Trim();
+                    var len = text.Length;
+                    int colon = -1;
+                    UInt16 ch = 0;
+                    for (int i = 0; i < len; i++)
+                    {
+                        if (text[i] >= '0' && text[i] <= '9')
+                        {
+                            ch *= 10;
+                            ch += (byte)((byte)text[i] - (byte)'0');
+                        }
+                        else
+                        {
+                            if (text[i] == ':')
+                                colon = i;
+                            break;
+                        }
+                    }
+                    if (ch <= 255 && Ribbon.RIBBON.chIdx[b].ContainsKey((byte)ch))
+                    {
+                        c = (byte)ch;
+                        var chapter = Ribbon.RIBBON.chIdx[b][c];
+                        this.info.Text += ("\nChapter " + ch.ToString() + " has " + ((uint)chapter.vcnt).ToString() + " verses");
+
+                        if (colon > 0 && colon+1 < len)
+                        {
+                            UInt16 val;
+                            byte[] vals = new byte[2];
+                             var sections = text.Substring(colon + 1).Split(',');
+                            foreach (var section in sections)
+                            {
+                                var range = section.Split('-');
+                                if (range.Length == 1)
+                                {
+                                    bad = range[0];
+                                    if (UInt16.TryParse(bad, out val) && val <= chapter.vcnt && val >= 1)
+                                    {
+                                        list.Add((byte)val);
+                                        bad = null;
+                                    }
+                                    else break;
+                                }
+                                else if (range.Length == 2)
+                                {
+                                    vals[0] = vals[1] = 0;
+                                    for (int i = 0; i < 2; i++)
+                                    {
+                                        bad = range[i];
+                                        if (UInt16.TryParse(bad, out val) && val <= chapter.vcnt && val >= 1)
+                                        {
+                                            vals[i] = (byte)val;
+                                            bad = null;
+                                        }
+                                        else break;
+                                    }
+                                    if (bad != null)
+                                        break;
+                                    if (vals[0] > vals[1])
+                                    {
+                                        error = "Invalid verse range specified";
+                                        break;
+                                    }
+                                    for (byte v = vals[0]; v <= vals[1]; v++)
+                                    {
+                                        list.Add(v);
+                                    }
+                                }
+                            }
+                            if (error != null)
+                            {
+                                this.info.Text = error;
+                            }
+                            else if (bad != null)
+                            {
+                                this.info.Text = "'" + bad + "' could not be interpretted as a verse (or it is out of range for "
+                                    + item.Content.ToString()
+                                    + " Chapter " + ch.ToString();
+                            }
+                        }
+                        else
+                        {
+                            for (byte v = 1; v <= chapter.vcnt; v++)
+                                list.Add(v);
+                        }
+                    }
+                    else
+                    {
+                        this.info.Text = "You must specify a chapter between 1 and " + ((uint)book.ccnt).ToString() + " (inclusive)";
+                    }
+                }
+                if (bad == null && error == null)
+                {
+                    this.WriteVerseSpec();
+
+                    bool first = true;
+                    int prev = 0;
+                    foreach (var v in list)
+                    {
+                        this.WriteVerse(b, c, v, true, first || (v == ++prev));
+                        prev = v;
+                        first = false;
+                    }
+                    dynamic rng = Ribbon.AVX.Application.ActiveDocument.Range();
+                    rng.Collapse(Word.WdCollapseDirection.wdCollapseEnd);
+                    rng.Text = "\n";
+
+                    this.Close();
+                }
+            }
+            else
+            {
+                this.info.Text = "You must first select a book from the list prior to attempting to inserting verses.";
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.KeyUp += new KeyEventHandler(MainWindow_KeyUp);
+        }
+
+        void MainWindow_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                button_Click(null, null);
+            }
+        }
+    }
+}
